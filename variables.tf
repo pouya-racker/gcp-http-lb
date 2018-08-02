8ec00a8e2a15f435f055308ca28003c080a0917b
Pouya Naghizadeh <pouya.naghizadeh@rackspace.com>
2018-08-02 11:37:19 -0700

    no default value for shared_vpc_enabled

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable project {
 
 variable shared_vpc_enabled {
   description = "Set to true if resource is being built in a shared-vpc"
-  default     = true
+  default     = ""
 }
 
 variable shared_vpc_project {
